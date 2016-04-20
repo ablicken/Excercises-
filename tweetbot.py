deee4b1b879ba54ebec9f040ae731fbbda28fec8
Ana Blickenstaff <anablickenstaff@Anas-MacBook-Air.local>
2016-04-20 17:24:35 -0400

    added tweetbot.py and tweetbot2.py

---
+++ tweetbot.py
@@ -0,0 +1,16 @@
+from twython import Twython, TwythonError
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
+twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+try:
+    twitter.update_status(status='See how easy this was?')
+except TwythonError as e:
+    print e
+sleep(600)
+
+# read from csv 
